2ad5bcb11bd1c9a3bc20c40b2844671f28586d26
seanhoyt963 <seanhoyt963@gmail.com>
2016-02-20 23:16:15 -0500

    Updates
    Most of these changes are from Ashyx!
    
    List of changes:
    Update prebuilt kernel
    Cleanup twrp flags
    Proper partition sizes
    Update recovery fstab and remove extra one.
    Add empty omni.dependencies
    
    Change-Id: I3a30ddafbc201811335e4b470911618929f1f67a

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter chagallwifi,$(TARGET_DEVICE)),)
+ifneq ($(filter chagallxx,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
